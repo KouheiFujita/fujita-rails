578b5c392ba1c0ff737dccbf04602491e3f4b54d
Kouhei Fujita <kouheifujita2017@gmail.com>
2017-08-07 08:01:18 +0000

    フォームで送信

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   
     get 'helo/index'
     get 'helo', to: 'helo#index'
+    
     get 'helo/other'
+    
+    post 'helo', to: 'helo#index'
+    post 'helo/index'
 end
