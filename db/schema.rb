83b0400928ddd91ec2ec5678a40fb7859a4ce956
Kouhei Fujita <kouheifujita2017@gmail.com>
2017-08-16 04:29:13 +0000

    Add Division_id To Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816041822) do
+ActiveRecord::Schema.define(version: 20170816042712) do
 
   create_table "blogconfigs", force: :cascade do |t|
     t.text "title"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20170816041822) do
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "division_id"
   end
 
 end
